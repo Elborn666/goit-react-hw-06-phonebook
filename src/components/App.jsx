417f20cac4825c19445292bb279760fb2e699035
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-06-30 22:03:38 +0300

    виправив помилки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,18 @@
-import { useState, useEffect } from 'react';
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList"
 import { Filter } from "./Filter/Filter"
-import Notiflix from 'notiflix';
 import { Container, Title, Heading2 } from './App.styled'
 
 
-const initialContactsList = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
-function App() {
-
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) ?? (initialContactsList)
-  );
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = (newContact) => {
-
-      const existingContact = contacts.find(
-      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    );
-    if (existingContact) {
-      Notiflix.Report.warning(
-        'Alert',
-        `Contact with name ${newContact.name} already exists!`,
-        'Ok'
-      );      
-      return;
-    }
-    
-  setContacts((prevContacts) => [...prevContacts, newContact]);  
-};
-
-const deleteContact = (id) => {
-  setContacts((prevContacts) =>
-    prevContacts.filter((contact) => contact.id !== id)
-  );  
-};
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  return (
+export const App = () => {
+    return (
     <Container>
       <Title>Phonebook</Title>
-      <ContactForm addContact={addContact} contacts={contacts} />
+      <ContactForm/>
 
       <Heading2>Contacts</Heading2>
-      <Filter filter={filter} setFilter={setFilter} />
-      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+      <Filter/>
+      <ContactList/>
     </Container>
   );
 }
-
-export default App;
